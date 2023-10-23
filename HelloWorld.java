75fbde1a06bdcea5b007d9fccc37ee8e4d8ec61c
TheSkyQAQ <1669272093@QQ.COM>
2023-10-23 20:42:22 +0800

    zhfshuaibi

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("cnm zhf");
+    }
+
+    //写一个方法循环输出1-10
+    public static void printNum() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
